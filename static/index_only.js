e03b999812f7bbb4d66822b3acec08d287e6bdbb
DisturbedEye <itrishakov81@bk.ru>
2023-05-10 12:52:31 +0700

    animations + pages + icons update

---
+++ static/index_only.js
@@ -0,0 +1,20 @@
+const dom = document;
+if (!sessionStorage.getItem("session_start")){
+    header = dom.querySelector("header");
+    banner_text = dom.querySelector(".banner__container");
+
+    sessionStorage.setItem("session_start", "1");
+    headerstyle = header.style;
+    bannertextstyle = banner_text.style;
+    headerstyle.top = "-10vh";
+    headerstyle.animation = "1s slideintop alternate";
+    headerstyle.animationDelay = "1s";
+    bannertextstyle.visibility = "hidden";
+    bannertextstyle.animation = "1s textslide alternate";
+    bannertextstyle.animationDelay = "1s";
+
+    setTimeout(() => {
+        headerstyle.top = "0";
+        bannertextstyle.visibility = "visible";
+    }, 1000);
+};
