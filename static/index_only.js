835e73d8a5e14ce965133347f7689384e50b08f1
DisturbedEye <itrishakov81@bk.ru>
2023-05-11 22:01:24 +0700

    catalogue + branches update

--- static/index_only.js
+++ static/index_only.js
@@ -1,3 +1,16 @@
+if (!sessionStorage.getItem("indexCarID1") || !sessionStorage.getItem("indexCarID2")){
+    let rn1 = Math.round(Math.random()*11+1);
+    let rn2 = Math.round(Math.random()*11+1);
+    sessionStorage.setItem("indexCarID1", rn1.toString());
+    sessionStorage.setItem("indexCarID2", rn2.toString());
+}
+if(sessionStorage.getItem("indexCarID1") && sessionStorage.getItem("indexCarID2")){
+    const cars = dom.querySelectorAll(".search-cars");
+    const n1 = sessionStorage.getItem("indexCarID1");
+    const n2 = sessionStorage.getItem("indexCarID2");
+    cars[0].src = "media/images/cars/sell/car".concat(n1).concat(".png")
+    cars[1].src = "media/images/cars/sell/car".concat(n2).concat(".png")
+}
 if (!sessionStorage.getItem("session_start")){
     header = dom.querySelector("header");
     banner_text = dom.querySelector(".banner__container");
